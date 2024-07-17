d5713736cbca16301a3df4650a594d02e5f4b9a9
Sandeep Maurya <sandeep886626@gmail.com>
2024-07-17 23:10:16 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Sandeep Maurya on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
